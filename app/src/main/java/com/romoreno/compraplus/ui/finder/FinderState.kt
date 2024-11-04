d49369649166d09e37c56bbbbfab4c7950c12283
Roberto Moreno <morenomunozroberto@gmail.com>
2024-11-04 23:31:23 +0100

    chore: remove unused class

--- app/src/main/java/com/romoreno/compraplus/ui/finder/FinderState.kt
+++
@@ -1,9 +0,0 @@
-package com.romoreno.compraplus.ui.finder
-
-sealed class FinderState {
-
-    data class Success(val products:String):FinderState()
-    data class Error(val error:String):FinderState()
-    data object Loading:FinderState()
-
-}
